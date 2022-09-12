e7cf03d815af17b826b17669e8b8822300cf6c34
Ilayaperumal <113359155+Ilayaperumal@users.noreply.github.com>
2022-09-13 00:39:18 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import this
+
+
+# In[2]:
+
+
+print("this is my first prog")
+
+
+# In[3]:
+
+
+print("my name is sahaana")
+
+
+# In[4]:
+
+
+22/7
+
+
+# In[5]:
+
+
+75*50
+
+
+# In[6]:
+
+
+a = 10
+
+
+# In[7]:
+
+
+a
+
+
+# In[8]:
+
+
+B=20
+
+
+# In[9]:
+
+
+B
+
+
+# In[10]:
+
+
+C = 3.14
+
+
+# In[11]:
+
+
+type(a)
+
+
+# In[12]:
+
+
+type(c)
+
+
+# In[13]:
+
+
+type(C)
+
+
+# In[24]:
+
+
+type(D)
+
+
+# In[16]:
+
+
+type(D)
+
+
+# In[17]:
+
+
+e = 5+4j
+
+
+# In[18]:
+
+
+type(e)
+
+
+# In[20]:
+
+
+X = "lyg;ihiohpihpihpihjpohjnp"
+
+
+# In[22]:
+
+
+h = True
+
+
+# In[23]:
+
+
+type(h)
+
+
+# In[25]:
+
+
+e.imag
+
+
+# In[26]:
+
+
+e.real
+
+
+# In[27]:
+
+
+e.conjugate
+
+
+# In[28]:
+
+
+D.capitalize
+
+
+# In[29]:
+
+
+D = "ilayaperumal"
+
+
+# In[30]:
+
+
+D.capitalize
+
+
+# In[31]:
+
+
+E = "K"
+
+
+# In[32]:
+
+
+D + E
+
+
+# In[33]:
+
+
+D.translate
+
+
+# In[34]:
+
+
+True + True
+
+
+# In[35]:
+
+
+True + False
+
+
+# In[36]:
+
+
+False + False
+
+
+# In[ ]:
+
+
+True +4
+
+
+# In[ ]:
+
+
+True + True
+
+
+# In[1]:
+
+
+True + True
+
+
+# In[2]:
+
+
+input()
+
+
+# In[3]:
+
+
+x = input()
+
+
+# In[4]:
+
+
+y = 45
+
+
+# In[5]:
+
+
+x + y
+
+
+# In[6]:
+
+
+int(x) + y
+
+
+# In[ ]:
+
+
+
+
