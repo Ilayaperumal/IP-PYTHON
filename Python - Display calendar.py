38eac03bbb2a50d3b187cc9f2a65383a627665e6
Ilayaperumal <113359155+Ilayaperumal@users.noreply.github.com>
2022-09-16 23:36:30 +0530

    Add files via upload

---
+++ Python - Display calendar.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+
+# Python program to display calendar of
+# given year
+ 
+# import module
+import calendar
+ 
+yy = 2022
+ 
+# display the calendar
+print(calendar.calendar(yy))
+
+
+# In[ ]:
+
+
+
+
