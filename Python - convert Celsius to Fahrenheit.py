3eb863ca606fd3c602f1b81396b1e8ffde13e741
Ilayaperumal <113359155+Ilayaperumal@users.noreply.github.com>
2022-09-16 23:21:04 +0530

    Add files via upload

---
+++ Python - convert Celsius to Fahrenheit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Getting fahrenheit input from the user
+fahrenheit = float(input("Enter value in fahrenheit: "))
+
+# calculate celsius
+celsius = (fahrenheit * 1.8) + 32
+print('%0.2f fahrenheit is equal to %0.2f celsius' %(fahrenheit, celsius))
+
+
+# In[ ]:
+
+
+
+
