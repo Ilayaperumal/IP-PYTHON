6e59529b3f162556d69bd038ed72da97a9a5d283
Ilayaperumal <113359155+Ilayaperumal@users.noreply.github.com>
2022-09-17 22:43:59 +0530

    Add files via upload

---
+++ Python - Leap year or not.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+# Python program to check if year is a leap year or not
+
+year = 2000
+
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
+else:
+    print("{0} is not a leap year".format(year))
+
+
+# In[11]:
+
+
+
+# Python program to check leap year or not
+def checkYear(year):
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                return True
+            else:
+                return False
+        else:
+             return True
+    else:
+        return False
+ 
+# Driver Code
+year = 2000
+if(checkYear(year)):
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
+
+
+# In[ ]:
+
+
+
+
